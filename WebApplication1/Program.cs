8f80084f6b497f7618e6f468941743cb7b993ca1
mrpine19 <mrpine19@gmail.com>
2026-09-05 20:19:59 -0300

    feat: adicionando o endpoint /health

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -26,5 +28,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health");
 
 app.Run();
